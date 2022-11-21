e6fedd253387c8c5415bcb2cd577d5e6c3eb7cdf
Aditya-Amgaonkar <adityaamgaonkar@live.com>
2022-11-21 22:45:09 +0530

    Basic JWT experiment

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+    from app import app
+
+    app.run()
+
+    # with app.app_context():
+    #     from app import db
+    #     db.create_all()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
